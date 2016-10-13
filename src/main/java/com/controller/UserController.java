5d41d02300de40f7f46c8b5be74feebd7c55bb3d
ma <gatehate@github.com>
2016-10-14 01:02:52 +0800

    Initial commit

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.controller;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.model.User;
+import com.service.IUserService;
+
+/**
+ * Created by ma on 2016/10/13.
+ */
+    @Controller
+    @RequestMapping("/user")
+    public class UserController {
+
+        @Resource
+        private IUserService userService;
+
+        @RequestMapping("/findAllUser")
+        public String findAllUser(HttpServletRequest request){
+            List<User> listUser =  userService.findAllUser();
+            request.setAttribute("listUser", listUser);
+            return "/allUser";
+        }
+    }
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<h5><a href="<%=basePath%>user/findAllUser">进入用户管理页</a></h5>
+</body>
+</html>
