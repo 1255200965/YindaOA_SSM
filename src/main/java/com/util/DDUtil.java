535aca2561d694886d2c68a1332888a87acab06f
LiuLiren <ustcllr@163.com>
2016-10-28 10:03:06 +0800

    处理冲突

--- src/main/java/com/util/DDUtil.java
+++ src/main/java/com/util/DDUtil.java
@@ -6,6 +6,7 @@ import com.ddSdk.user.UserHelper;
 import com.ddSdk.utils.FileUtils;
 import com.dingtalk.open.client.ServiceFactory;
 import com.dingtalk.open.client.api.model.corp.CorpUserDetail;
+import com.dingtalk.open.client.api.model.corp.CorpUserList;
 import com.dingtalk.open.client.api.service.corp.CorpUserService;
 import com.model.StaffInfo;
 
@@ -16,10 +17,11 @@ import java.util.Map;
  * Created by ma on 2016/10/26.
  */
 public class DDUtil {
+
     public static String jsapiTicket = null;
     public static String accessToken = null;
 
-    public static String getAccessToken(){
+    public String getAccessToken(){
         try {
             if (accessToken == null) {
                 accessToken = AuthHelper.getAccessToken();
@@ -29,29 +31,46 @@ public class DDUtil {
         }
         return accessToken;
     }
-    public static String createUser(StaffInfo user){
-        String result = null;
-        try {
-            CorpUserDetail userDetail = ChangeToDD(user);
-            CorpUserService corpUserService = ServiceFactory.getInstance().getOpenService(CorpUserService.class);
-            //获取部门id
-            Map<Long, Long> orderInDepts = new HashMap<Long, Long>();
-            orderInDepts.put((long)orderInDepts.size(),Long.parseLong(user.getDepartment()));
-            //获取userid
-            result = corpUserService.createCorpUser(getAccessToken(), userDetail.getUserid(), userDetail.getName(), orderInDepts,
-                    userDetail.getDepartment(), userDetail.getPosition(), userDetail.getMobile(), userDetail.getTel(), userDetail.getWorkPlace(),
-                    userDetail.getRemark(), userDetail.getEmail(), userDetail.getJobnumber(),
-                    userDetail.getIsHide(), userDetail.getSenior(), userDetail.getExtattr());
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return result;
-       /* {
-            "errcode": 0,
-                "errmsg": "created",
-                "userid": "dedwefewfwe1231"
-        }*/
+
+    /**
+     * 先定一个小目标，通过部门id，得到创新部的所有成员
+     */
+    public CorpUserList getAllDepartMem(
+            String accessToken,
+            long departmentId,
+            Long offset,
+            Integer size,
+            String order)
+            throws Exception {
+
+        CorpUserService corpUserService = ServiceFactory.getInstance().getOpenService(CorpUserService.class);
+        return corpUserService.getCorpUserSimpleList(accessToken, departmentId,
+                offset, size, order);
     }
+
+//    public static String createUser(StaffInfo user){
+//        String result = null;
+//        try {
+//            CorpUserDetail userDetail = ChangeToDD(user);
+//            CorpUserService corpUserService = ServiceFactory.getInstance().getOpenService(CorpUserService.class);
+//            //获取部门id
+//            Map<Long, Long> orderInDepts = new HashMap<Long, Long>();
+//            orderInDepts.put((long)orderInDepts.size(),Long.parseLong(user.getDepartment()));
+//            //获取userid
+//            result = corpUserService.createCorpUser(getAccessToken(), userDetail.getUserid(), userDetail.getName(), orderInDepts,
+//                    userDetail.getDepartment(), userDetail.getPosition(), userDetail.getMobile(), userDetail.getTel(), userDetail.getWorkPlace(),
+//                    userDetail.getRemark(), userDetail.getEmail(), userDetail.getJobnumber(),
+//                    userDetail.getIsHide(), userDetail.getSenior(), userDetail.getExtattr());
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return result;
+//       /* {
+//            "errcode": 0,
+//                "errmsg": "created",
+//                "userid": "dedwefewfwe1231"
+//        }*/
+//    }
     public static String updateUser(StaffInfo user){
         String result = null;
         try {
@@ -88,16 +107,16 @@ public class DDUtil {
             "errmsg": "deleted"
         }*/
     }
-    public static StaffInfo getUserByID(String userid){
-        StaffInfo user = null;
-        try {
-            CorpUserDetail dduser = UserHelper.getUser(getAccessToken(), userid);
-            user = ChangeToLocal(dduser);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return user;
-    }
+//    public static StaffInfo getUserByID(String userid){
+//        StaffInfo user = null;
+//        try {
+//            CorpUserDetail dduser = UserHelper.getUser(getAccessToken(), userid);
+//            user = ChangeToLocal(dduser);
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return user;
+//    }
 
     //转换成本地用户类
     public static StaffInfo ChangeToLocal(CorpUserDetail user){
